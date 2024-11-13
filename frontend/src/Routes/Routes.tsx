84d9b95cb932448217a2d6ab97db2380027b32b6
Brandonsaurio21001 <brandonbl2001@estudiantec.cr>
2024-11-13 15:42:44 -0600

    Update Routes.tsx

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -1,78 +1,85 @@
 import { createBrowserRouter } from "react-router-dom";
 
-// Página inicial
+// Páginas comunes
 import App from "../App";
 import HomePage from "../Views/HomePage";
 import LoginPage from "../Views/LoginPage";
 import SignUpPage from "../Views/SignUpPage";
 import ChangePassword from "../Views/ChangePassword";
-
 import MainPage from "../Views/MainPage";
 import ModifyUserPage from "../Views/ModifyUserPage";
 import ProjectPage from "../Views/ProjectPage";
 import NewProjectPage from "../Views/NewProjectPage";
 import SearchedProjectPage from "../Views/SearchedProjectPage";
 
-// Admin Pages
+// Páginas de administración
 import DonationsManagement from "../Views/admin/DonationsManagement";
 import UserManagement from "../Views/admin/UserManagement";
 import ProjectValidation from "../Views/admin/ProjectValidation";
 import EventConfiguration from "../Views/admin/EventConfiguration";
 
-// Acá nos encargaremos de manejar las rutas dentro de la página
+// Configuración de rutas
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      // Rutas públicas
       {
-        // Landing page
         path: "",
         element: <HomePage />,
       },
       {
-        // Página de Login
         path: "/login",
         element: <LoginPage />,
       },
       {
-        // Página de SignUp
         path: "/signup",
         element: <SignUpPage />,
       },
       {
-        // Página de contraseña
         path: "/change-password",
         element: <ChangePassword />,
       },
+      
+      // Rutas de usuario
       {
-        // Página para modificar los datos del usuario
         path: "/user-settings",
         element: <ModifyUserPage />,
       },
-
-      /* Sección de main-page y búsqueda respectiva */
       {
-        // Página de inicio de usuario
         path: "/main-page",
         element: <MainPage />,
       },
-
-      /* Sección de Proyectos */
       {
-        // Página de creación de Proyectos
         path: "/new-project",
         element: <NewProjectPage />,
       },
       {
-        // Página de proyecto de forma individual
         path: "/project",
         element: <ProjectPage />,
       },
       {
-        // Páginas específicas para proyectos
         path: "/search/categories",
         element: <SearchedProjectPage />,
+      },
+
+      // Rutas de administrador (solo accesibles para usuarios con rol "admin")
+      {
+        path: "/admin/donations-management",
+        element: <DonationsManagement />,
+      },
+      {
+        path: "/admin/user-management",
+        element: <UserManagement />,
+      },
+      {
+        path: "/admin/validate-projects",
+        element: <ProjectValidation />,
+      },
+      {
+        path: "/admin/event-configuration",
+        element: <EventConfiguration />,
       },
     ],
   },
