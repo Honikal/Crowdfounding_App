c42b2008ce71a2d6589a4f2c254ba03f555c3c97
Brandonsaurio21001 <brandonbl2001@estudiantec.cr>
2024-11-13 15:23:45 -0600

    merge con vale
    hola

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 
-//Pagina inicial
+// Página inicial
 import App from "../App";
 import HomePage from "../Views/HomePage";
 import LoginPage from "../Views/LoginPage";
@@ -13,97 +13,67 @@ import ProjectPage from "../Views/ProjectPage";
 import NewProjectPage from "../Views/NewProjectPage";
 import SearchedProjectPage from "../Views/SearchedProjectPage";
 
-
-//Admin Pages
+// Admin Pages
 import DonationsManagement from "../Views/admin/DonationsManagement";
 import UserManagement from "../Views/admin/UserManagement";
 import ProjectValidation from "../Views/admin/ProjectValidation";
 import EventConfiguration from "../Views/admin/EventConfiguration";
 
-
-//Acá nos encargaremos de manejar las rutas dentro de la página
+// Acá nos encargaremos de manejar las rutas dentro de la página
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        children: [
-            {
-                //Landing page
-                path: "",
-                element: <HomePage/>
-            },
-            {
-                //Página de Login
-                path: "/login",
-                element: <LoginPage/>
-            },
-            {
-                //Página de SignUp
-                path: "/signup",
-                element: <SignUpPage/>
-            },
-            {
-                //Página de contraseña
-                path: "/change-password",
-                element: <ChangePassword/>
-            },
-            {
-                //Página para modificar los datos del usuario
-                path: "/user-settings",
-                element: <ModifyUserPage/>
-            },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        // Landing page
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        // Página de Login
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        // Página de SignUp
+        path: "/signup",
+        element: <SignUpPage />,
+      },
+      {
+        // Página de contraseña
+        path: "/change-password",
+        element: <ChangePassword />,
+      },
+      {
+        // Página para modificar los datos del usuario
+        path: "/user-settings",
+        element: <ModifyUserPage />,
+      },
 
-            /*Sección de main-page y búsqueda respectiva*/
-            {
-                //Página de inicio de usuario
-                path: "/main-page",
-                element: <MainPage/>
-            },
+      /* Sección de main-page y búsqueda respectiva */
+      {
+        // Página de inicio de usuario
+        path: "/main-page",
+        element: <MainPage />,
+      },
 
-<<<<<<< HEAD
-
-            /*Sección de Proyectos*/
-            {
-                //Página de creación de Proyectos
-                path: "/new-project",
-                element: <NewProjectPage/>
-            },
-            {
-                //Página de proyecto de forma individual
-                path: "/project",
-                element: <ProjectPage/>
-            },
-            {
-                //Páginas específicas para proyectos
-                path: "/search/categories",
-                element: <SearchedProjectPage/>
-=======
-            {
-                //Página de administración de donaciones
-                path: "/donations-management",
-                element: <DonationsManagement/>
-            },
-
-            {
-                //Página de administración de usuarios
-                path: "/user-management",
-                element: <UserManagement/>
-            },
-
-            {
-                //Página de validación de proyectos
-                path: "/validate-projects",
-                element: <ProjectValidation/>
-            },
-             
-            {
-                //Página de configuración de eventos
-                path: "/configure-events",
-                element: <EventConfiguration/>
->>>>>>> Valeria
-            }
-
-
-        ]        
-    }
+      /* Sección de Proyectos */
+      {
+        // Página de creación de Proyectos
+        path: "/new-project",
+        element: <NewProjectPage />,
+      },
+      {
+        // Página de proyecto de forma individual
+        path: "/project",
+        element: <ProjectPage />,
+      },
+      {
+        // Páginas específicas para proyectos
+        path: "/search/categories",
+        element: <SearchedProjectPage />,
+      },
+    ],
+  },
 ]);
